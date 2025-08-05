2b9fb2c8d0afa2d43e9082b58ef83ce08a4af0ec
Nitin kachera <144806801+NitinXD1@users.noreply.github.com>
2025-08-05 16:14:09 +0530

    Time: 1 ms (17.44%) | Memory: 40.7 MB (98.48%) - LeetSync

---
+++ 216-combination-sum-iii/combination-sum-iii.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        boolean[] used = new boolean[10];
+
+        HashSet<List<Integer>> ans = new HashSet<>();
+
+        helper(1,ans,new ArrayList<>(),k,n,used);
+
+        List<List<Integer>> main = new ArrayList<>(ans);
+
+        return main;
+    }
+
+    public void helper(int curr,HashSet<List<Integer>> ans,List<Integer> currLs,int k,int n,boolean[] used){
+        if(k == 0){
+            if(n == 0){
+                ans.add(new ArrayList<>(currLs));
+                return;
+            }
+        }
+
+
+        for(int i=curr;i<10;i++){
+            if(!used[i]){
+                
+                used[i] = true;
+                currLs.add(i);
+
+                helper(i+1,ans,currLs,k-1,n-i,used);
+                
+                currLs.remove(currLs.size()-1);
+                used[i] = false;
+            }
+        }
+    }
+}
