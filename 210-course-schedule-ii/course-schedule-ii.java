a022e5c605e5fec2345e93dfd8e2821c854d4535
Nitin kachera <144806801+NitinXD1@users.noreply.github.com>
2025-08-31 23:24:37 +0530

    Time: 4 ms (91.41%) | Memory: 45.9 MB (74.54%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,41 +1,52 @@
 class Solution {
-    public int[] findOrder(int n, int[][] preq) {
-        List<List<Integer>> adj = new ArrayList<>();
+    public int[] findOrder(int numCourses, int[][] preq) {
+       List<List<Integer>> adj = new ArrayList<>();
 
-        for(int i=0;i<n;i++){
+        for(int i=0;i<numCourses;i++){
             adj.add(new ArrayList<>());
         }
 
-        int[] inDegree = new int[n];
-
         for(int[] i:preq){
             adj.get(i[1]).add(i[0]);
-            inDegree[i[0]]++;
         }
 
+        boolean[] vis = new boolean[numCourses];
+        boolean[] pathVis = new boolean[numCourses];
 
-        Queue<Integer> q = new LinkedList<>();
+        Stack<Integer> st = new Stack<>();
 
-        for(int i=0;i<n;i++){
-            if(inDegree[i] == 0)q.offer(i);
-        }
-        
-        List<Integer> ans = new ArrayList<>();
-
-        while(!q.isEmpty()){
-            int node = q.poll();
-            ans.add(node);
-
-            for(int adjNode : adj.get(node)){
-                if(--inDegree[adjNode] == 0){
-                    q.offer(adjNode);
-                }
+        for(int i=0;i<numCourses;i++){
+            if(!vis[i]){
+                if(!helper(i,vis,adj,pathVis,st))return new int[0];
             }
         }
 
-        if(ans.size() != n)return new int[0];
+        int[] ans = new int[numCourses];
+        
+        int i=0;
 
-        return ans.stream().mapToInt(i -> i).toArray();        
+        while(!st.isEmpty()){
+            ans[i] = st.pop();
+            i++;
+        }
+
+        return ans;
     }
 
+    public boolean helper(int index,boolean[] vis,List<List<Integer>> adj,boolean[] pathVis,Stack<Integer> st){
+        vis[index] = true;
+        pathVis[index] = true;
+        
+
+        for(int adjIndex : adj.get(index)){
+            if(!vis[adjIndex]){
+                if(!helper(adjIndex,vis,adj,pathVis,st))return false;
+            }
+            else if(pathVis[adjIndex])return false;
+        }
+
+        pathVis[index] = false;
+        st.add(index);
+        return true;
+    }
 }
