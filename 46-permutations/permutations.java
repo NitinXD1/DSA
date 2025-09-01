f1084a945557f92a1b83b4b2c06a2c9078926e18
Nitin kachera <144806801+NitinXD1@users.noreply.github.com>
2025-09-01 12:52:56 +0530

    Time: 2 ms (34.47%) | Memory: 44.3 MB (97.27%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,22 +1,22 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         int n = nums.length;
-        HashMap<Integer,Integer> hash = new HashMap<>();
+        HashSet<Integer> hash = new HashSet<>();
 
         List<List<Integer>> ans = new ArrayList<>();
         helper(nums,hash,new ArrayList<>(),ans);
         return ans;
     }
 
-    public void helper(int[] nums,HashMap<Integer,Integer> hash,List<Integer> curr,List<List<Integer>> ans){
+    public void helper(int[] nums,HashSet<Integer> hash,List<Integer> curr,List<List<Integer>> ans){
         if(curr.size() == nums.length){
             ans.add(new ArrayList<>(curr));
         }
 
         for(int i=0;i<nums.length;i++){
-            if(!hash.containsKey(nums[i])){
+            if(!hash.contains(nums[i])){
                 curr.add(nums[i]);
-                hash.put(nums[i],1);
+                hash.add(nums[i]);
                 helper(nums,hash,curr,ans);
                 curr.remove(curr.size()-1);
                 hash.remove(nums[i]);
