752202a2263b231e6f198b1534e2fe89febaa60d
Nitin kachera <144806801+NitinXD1@users.noreply.github.com>
2025-09-03 17:32:48 +0530

    Time: 33 ms (11.63%) | Memory: 45.8 MB (30.78%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -4,41 +4,38 @@ class Solution {
 
         if(grid[0][0] == 1 || grid[n-1][n-1] == 1)return -1;
 
-        boolean[][] vis = new boolean[n][n];
-        // for(int[] i:vis)Arrays.fill(i,Integer.MAX_VALUE);
+        int[][] dist = new int[n][n];
+        for(int[] i:dist)Arrays.fill(i,Integer.MAX_VALUE);
 
-        Queue<int[]> q = new LinkedList<>();
+        PriorityQueue<int[]> q = new PriorityQueue<>((a,b) -> Integer.compare(a[2],b[2]));
 
-        q.offer(new int[]{0,0});
+        q.offer(new int[]{0,0,1});
 
         int[] drow = {1,-1,-1,1,0,-1,0,1};
         int[] dcol = {1,-1,1,-1,1,0,-1,0};
 
-        int cnt = 1;
         while(!q.isEmpty()){
-            int size = q.size();
+            
+            int[] curr = q.poll();
+            int r = curr[0];
+            int c = curr[1];
+            int d = curr[2];
 
-            for(int k=0;k<size;k++){
-                int[] curr = q.poll();
-                int r = curr[0];
-                int c = curr[1];
+            if(r == n-1 && c == n-1)return d;
 
-                if(r == n-1 && c == n-1)return cnt;
+            if(dist[r][c] < d)continue;
 
-                if(vis[r][c])continue;
-                
-                vis[r][c] = true;
+            for(int i=0;i<8;i++){
+                int adjR = r + drow[i];
+                int adjC = c + dcol[i];
 
-                for(int i=0;i<8;i++){
-                    int adjR = r + drow[i];
-                    int adjC = c + dcol[i];
-
-                    if(adjR >= 0 && adjR < n && adjC >= 0 && adjC < n && grid[adjR][adjC] == 0){
-                        q.offer(new int[]{adjR,adjC});
+                if(adjR >= 0 && adjR < n && adjC >= 0 && adjC < n && grid[adjR][adjC] == 0){
+                    if(d + 1 < dist[adjR][adjC]){
+                        dist[adjR][adjC] = d+1;
+                        q.offer(new int[]{adjR,adjC,d+1});
                     }
                 }
             }
-            cnt++;
         }
 
         return -1; 
