107bc189c91e6410c9737340bb69d67b49b531f8
Nitin kachera <144806801+NitinXD1@users.noreply.github.com>
2025-09-04 21:15:41 +0530

    Time: 10 ms (80.65%) | Memory: 48.8 MB (62.10%) - LeetSync

---
+++ 744-network-delay-time/network-delay-time.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int networkDelayTime(int[][] times, int n, int k) {
+        List<List<int[]>> adj = new ArrayList<>();
+
+        for(int i=0;i<=n;i++)adj.add(new ArrayList<>());
+
+        for(int[] i:times)adj.get(i[0]).add(new int[]{i[1],i[2]});
+
+        int[] dist = new int[n+1];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+
+        PriorityQueue<int[]> q = new PriorityQueue<>((a,b) -> Integer.compare(a[1],b[1]));
+
+        q.offer(new int[]{k,0});
+
+        dist[k] = 0;
+
+        while(!q.isEmpty()){
+            int[] curr = q.poll();
+            int node = curr[0];
+            int cost = curr[1];
+
+            for(int[] adjArr : adj.get(node)){
+                int adjNode = adjArr[0];
+                int adjCost = adjArr[1];
+
+                int newCost = cost + adjCost;
+
+                if(newCost < dist[adjNode]){
+                    dist[adjNode] = newCost;
+                    q.offer(new int[]{adjNode,newCost});
+                }
+            }
+        }
+
+        int max = 0;
+
+        for(int i=1;i<=n;i++){
+            max = Math.max(max,dist[i]);
+        }
+
+        return max == Integer.MAX_VALUE ? -1 : max;
+    }
+}
