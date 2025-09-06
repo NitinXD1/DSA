b63482f07620d96f603c2df512ed4d11f77049ec
Nitin kachera <144806801+NitinXD1@users.noreply.github.com>
2025-09-06 17:10:40 +0530

    Time: 6 ms (10.76%) | Memory: 47.5 MB (81.67%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -4,20 +4,20 @@ class Solution {
         int m = grid[0].length;
         
         int[][] dp = new int[n][m];
-        for(int[] i:dp)Arrays.fill(i,-1);
 
-        return helper(0,0,n,m,grid,dp);
-    }
+        dp[0][0] = grid[0][0];
 
-    public int helper(int r,int c,int n,int m,int[][] grid,int[][] dp){
-        if(r == n-1 && c == m-1)return grid[n-1][m-1];
-        if(r == n || c == m)return (int)(1e9);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i == 0 && j == 0)continue;
 
-        if(dp[r][c] != -1)return dp[r][c];
+                int left = j == 0 ? (int)(1e9) : dp[i][j-1];  
+                int up = i == 0 ? (int)(1e9) : dp[i-1][j];
 
-        int down = helper(r+1,c,n,m,grid,dp);
-        int right = helper(r,c+1,n,m,grid,dp);
+                dp[i][j] = Math.min(left,up) + grid[i][j];
+            }
+        }
 
-        return dp[r][c] = Math.min(down,right) + grid[r][c];
+        return dp[n-1][m-1];
     }
 }
