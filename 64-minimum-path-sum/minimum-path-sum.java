0ea42ccf7c2e2d9e9d12f168b3645574638658f3
Nitin kachera <144806801+NitinXD1@users.noreply.github.com>
2025-09-06 17:07:49 +0530

    Time: 1 ms (99.75%) | Memory: 48 MB (21.79%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,38 +1,23 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> Integer.compare(a[2],b[2]));
         int n = grid.length;
         int m = grid[0].length;
         
-        pq.offer(new int[]{0,0,grid[0][0]});
+        int[][] dp = new int[n][m];
+        for(int[] i:dp)Arrays.fill(i,-1);
 
-        boolean[][] vis = new boolean[n][m];
+        return helper(0,0,n,m,grid,dp);
+    }
 
-        int[] drow = {1,0};
-        int[] dcol = {0,1};
+    public int helper(int r,int c,int n,int m,int[][] grid,int[][] dp){
+        if(r == n-1 && c == m-1)return grid[n-1][m-1];
+        if(r == n || c == m)return (int)(1e9);
 
-        while(!pq.isEmpty()){
-            int[] curr = pq.poll();
-            int r = curr[0];
-            int c = curr[1];
-            int cost = curr[2];
+        if(dp[r][c] != -1)return dp[r][c];
 
-            if(vis[r][c])continue;
-            vis[r][c] = true;
+        int down = helper(r+1,c,n,m,grid,dp);
+        int right = helper(r,c+1,n,m,grid,dp);
 
-            if(r == n-1 && c == m-1)return cost;
-
-            for(int i=0;i<2;i++){
-                int adjR = r + drow[i];
-                int adjC = c + dcol[i];
-
-                if(adjR >= 0 && adjR < n && adjC >= 0 && adjC < m && !vis[adjR][adjC]){
-                    int newCost = cost+grid[adjR][adjC];
-                    pq.offer(new int[]{adjR,adjC,newCost});
-                }
-            }
-        }
-        
-        return -1;
+        return dp[r][c] = Math.min(down,right) + grid[r][c];
     }
 }
