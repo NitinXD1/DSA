ced03c281d0e8950fc249f8246133732d7fcc13f
Nitin kachera <144806801+NitinXD1@users.noreply.github.com>
2025-09-06 17:37:05 +0530

    Time: 1 ms (15.93%) | Memory: 41.7 MB (82.02%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -6,22 +6,20 @@ class Solution {
         if(grid[0][0] == 1 || grid[n-1][m-1] == 1)return 0;
 
         int[][] dp = new int[n][m];
-        for(int[] i:dp)Arrays.fill(i,-1);
 
-        return helper(0,0,n,m,grid,dp);
-    }
+        dp[n-1][m-1] = 1;
 
-    public int helper(int r,int c,int n,int m,int[][] grid,int[][] dp){
-        if(r == n-1 && c == m-1)return 1;
-        if(r == n || c == m)return 0;
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(i == n-1 && j == m-1)continue;
 
-        if(grid[r][c] == 1)return 0;
+                int down = i == n-1 || grid[i+1][j] == 1 ? 0 : dp[i+1][j];
+                int right = j == m-1 || grid[i][j+1] == 1 ? 0 : dp[i][j+1];
 
-        if(dp[r][c] != -1)return dp[r][c];
+                dp[i][j] = down + right; 
+            }
+        }
 
-        int down = helper(r+1,c,n,m,grid,dp);
-        int right = helper(r,c+1,n,m,grid,dp);
-
-        return dp[r][c] = down + right;
+        return dp[0][0];
     }
 }
