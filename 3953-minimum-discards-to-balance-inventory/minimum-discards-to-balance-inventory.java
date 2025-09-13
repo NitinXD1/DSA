f894087e9bc6b08aae5661db4e62102dfb2d176c
Nitin kachera <144806801+NitinXD1@users.noreply.github.com>
2025-09-14 01:54:56 +0530

    Time: 43 ms (100.00%) | Memory: 71.6 MB (100.00%) - LeetSync

--- 3953-minimum-discards-to-balance-inventory/minimum-discards-to-balance-inventory.java
+++ 3953-minimum-discards-to-balance-inventory/minimum-discards-to-balance-inventory.java
@@ -1,26 +1,25 @@
 class Solution {
     public int minArrivalsToDiscard(int[] arrivals, int w, int m) {
+        Queue<int[]> dq = new LinkedList<>();
+        HashMap<Integer,Integer> hash = new HashMap<>();
+        int cnt = 0;
         int n = arrivals.length;
-        if (n == 0) return 0;
 
-        Map<Integer, Integer> map = new HashMap<>();
-        int[] kept = new int[n];
-        int dis = 0;
-
-        for (int i = 0; i < n; i++) {
-            int idx = i - w;
-            if (idx >= 0 && kept[idx] == 1) {
-                map.put(arrivals[idx], map.get(arrivals[idx]) - 1);
+        for(int i=0;i<n;i++){
+            if(dq.size() > 0 && dq.peek()[1] + w - 1< i){
+                int rem = dq.poll()[0];
+                hash.put(rem,hash.get(rem)-1);
             }
 
-            int t = arrivals[i];
-            if (map.getOrDefault(t, 0) < m) {
-                kept[i] = 1;
-                map.put(t, map.getOrDefault(t, 0) + 1);
-            } else {
-                dis++;
+            if(hash.containsKey(arrivals[i]) && hash.get(arrivals[i]) == m){
+                cnt++;
+                continue;
             }
+
+            hash.put(arrivals[i],hash.getOrDefault(arrivals[i],0)+1);
+            dq.offer(new int[]{arrivals[i],i});
         }
-        return dis;
+
+        return cnt;
     }
 }
