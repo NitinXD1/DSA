07e67162fca116b89a8c95a6e03d1426553d0447
Nitin kachera <144806801+NitinXD1@users.noreply.github.com>
2025-09-13 22:42:13 +0530

    Time: 51 ms (9.81%) | Memory: 62.8 MB (42.23%) - LeetSync

---
+++ 3953-minimum-discards-to-balance-inventory/minimum-discards-to-balance-inventory.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minArrivalsToDiscard(int[] arrivals, int w, int m) {
+        Deque<int[]> dq = new ArrayDeque<>();
+        HashMap<Integer,Integer> hash = new HashMap<>();
+        int cnt = 0;
+        int n = arrivals.length;
+
+        for(int i=0;i<n;i++){
+            if(dq.size() > 0 && dq.peekFirst()[1] + w - 1< i){
+                int rem = dq.pollFirst()[0];
+                hash.put(rem,hash.get(rem)-1);
+            }
+
+            if(hash.containsKey(arrivals[i]) && hash.get(arrivals[i]) == m){
+                cnt++;
+                continue;
+            }
+
+            hash.put(arrivals[i],hash.getOrDefault(arrivals[i],0)+1);
+            dq.offerLast(new int[]{arrivals[i],i});
+        }
+
+        return cnt;
+    }
+}
