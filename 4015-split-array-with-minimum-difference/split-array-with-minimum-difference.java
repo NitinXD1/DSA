b5f6dc873bee1c0a76a97ee9532199d6471921f5
Nitin kachera <144806801+NitinXD1@users.noreply.github.com>
2025-10-01 20:19:44 +0530

    Time: 2 ms (99.46%) | Memory: 62.3 MB (22.62%) - LeetSync

---
+++ 4015-split-array-with-minimum-difference/split-array-with-minimum-difference.java
@@ -0,0 +1,31 @@
+
+class Solution {
+    public long splitArray(int[] arr) {
+        int n = arr.length;
+        int l = 0, r = n - 1;
+        long lsum = 0, rsum = 0;
+
+        while (l < n - 1 && arr[l] < arr[l + 1]) {
+            lsum += arr[l];
+            l++;
+        }
+
+        while (r > 0 && arr[r - 1] > arr[r]) {
+            rsum += arr[r];
+            r--;
+        }
+
+        if (l==r){
+            long option1 = Math.abs((lsum + arr[l]) - rsum);
+            long option2 = Math.abs(lsum - (rsum + arr[l]));
+
+            return Math.min(option1, option2);
+        }
+        else if(arr[l]==arr[r] && r-l == 1){
+            long option1 = Math.abs((lsum  - rsum));
+            return option1;
+        }
+        
+        else  return -1;
+    }
+}
