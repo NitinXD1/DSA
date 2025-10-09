1a7dc2e0ccde20cec3ab44d5b004004893b3e3fe
Nitin kachera <144806801+NitinXD1@users.noreply.github.com>
2025-10-09 20:00:21 +0530

    Time: 52 ms (45.48%) | Memory: 73 MB (26.37%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,34 +1,30 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int[][][] dp = new int[n][2][2];
+        int[][] dp = new int[n][4];
 
-        for(int[][] i:dp){
-            for(int[] j:i){
-                Arrays.fill(j,-1);
-            }
-        }
+        for(int[] i:dp)Arrays.fill(i,-1);
 
-        return helper(0,1,1,prices,dp);
+        return helper(0,3,prices,dp);
     }
 
-    public int helper(int index,int count,int buy,int[] prices,int[][][] dp){
-        if(count == -1 || index == prices.length)return 0;
+    public int helper(int index,int buy,int[] prices,int[][] dp){
+        if(buy == -1 || index == prices.length)return 0;
 
-        if(dp[index][count][buy] != -1)return dp[index][count][buy];
+        if(dp[index][buy] != -1)return dp[index][buy];
 
-        int skip = helper(index+1,count,buy,prices,dp);
+        int skip = helper(index+1,buy,prices,dp);
         int profit = 0;
 
-        if(buy==1){
-            int invest = -prices[index] + helper(index+1,count,0,prices,dp);
+        if(buy%2==1){
+            int invest = -prices[index] + helper(index+1,buy-1,prices,dp);
             profit = Math.max(skip,invest);
         }
         else{
-            int sell = prices[index] + helper(index+1,count-1,1,prices,dp);
+            int sell = prices[index] + helper(index+1,buy-1,prices,dp);
             profit = Math.max(sell,skip);
         }
 
-        return dp[index][count][buy] = profit;
+        return dp[index][buy] = profit;
     }
 }
