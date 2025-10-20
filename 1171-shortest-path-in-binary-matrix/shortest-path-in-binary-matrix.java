965bcbee6b6fb230b9fcf40ef7952461e0609355
Nitin kachera <144806801+NitinXD1@users.noreply.github.com>
2025-10-20 15:46:24 +0530

    Time: 32 ms (12.46%) | Memory: 45.9 MB (32.78%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,43 +1,38 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
         int n = grid.length;
-
         if(grid[0][0] == 1 || grid[n-1][n-1] == 1)return -1;
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> Integer.compare(a[2],b[2]));
+        pq.offer(new int[]{0,0,1});
+
+        int[] drow = {0,1,0,-1,-1,-1,1,1};
+        int[] dcol = {1,0,-1,0,-1,1,1,-1};
 
         int[][] dist = new int[n][n];
         for(int[] i:dist)Arrays.fill(i,Integer.MAX_VALUE);
+        dist[0][0] = 1;
 
-        PriorityQueue<int[]> q = new PriorityQueue<>((a,b) -> Integer.compare(a[2],b[2]));
-
-        q.offer(new int[]{0,0,1});
-
-        int[] drow = {1,-1,-1,1,0,-1,0,1};
-        int[] dcol = {1,-1,1,-1,1,0,-1,0};
-
-        while(!q.isEmpty()){
-            
-            int[] curr = q.poll();
+        while(!pq.isEmpty()){
+            int[] curr = pq.poll();
             int r = curr[0];
             int c = curr[1];
-            int d = curr[2];
+            int s = curr[2];
 
-            if(r == n-1 && c == n-1)return d;
-
-            if(dist[r][c] < d)continue;
+            if(r == n-1 && c == n-1)return s;
+            if(dist[r][c] < s)continue;
 
             for(int i=0;i<8;i++){
                 int adjR = r + drow[i];
                 int adjC = c + dcol[i];
-
-                if(adjR >= 0 && adjR < n && adjC >= 0 && adjC < n && grid[adjR][adjC] == 0){
-                    if(d + 1 < dist[adjR][adjC]){
-                        dist[adjR][adjC] = d+1;
-                        q.offer(new int[]{adjR,adjC,d+1});
-                    }
+                
+                if(adjR >= 0 && adjR < n && adjC >= 0 && adjC < n && grid[adjR][adjC] == 0 && s + 1 < dist[adjR][adjC]){
+                    pq.offer(new int[]{adjR,adjC,s+1});
+                    dist[adjR][adjC] = s+1;
                 }
             }
         }
 
-        return -1; 
+        return -1;
     }
 }
