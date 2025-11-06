8340289742b9d75f07919738bdf7362c82d33ec1
Nitin kachera <144806801+NitinXD1@users.noreply.github.com>
2025-11-06 12:16:52 +0530

    Time: 133 ms (48.95%) | Memory: 274.7 MB (5.02%) - LeetSync

--- 3863-power-grid-maintenance/power-grid-maintenance.java
+++ 3863-power-grid-maintenance/power-grid-maintenance.java
@@ -3,21 +3,20 @@ import java.util.*;
 class Dsu {
     int[] parent;
     int[] size;
-    HashMap<Integer, PriorityQueue<Integer>> pqMap;
-    HashSet<Integer> offline;
+    HashMap<Integer, PriorityQueue<Integer>> hash;
+    HashSet<Integer> off;
 
     public Dsu(int n) {
         parent = new int[n + 1];
         size = new int[n + 1];
-        pqMap = new HashMap<>();
-        offline = new HashSet<>();
-
+        hash = new HashMap<>();
+        off = new HashSet<>();
         for (int i = 1; i <= n; i++) {
             parent[i] = i;
             size[i] = 1;
             PriorityQueue<Integer> pq = new PriorityQueue<>();
             pq.add(i);
-            pqMap.put(i, pq);
+            hash.put(i, pq);
         }
     }
 
@@ -27,39 +26,38 @@ class Dsu {
     }
 
     void union(int a, int b) {
-        int pa = findParent(a), pb = findParent(b);
-        if (pa == pb) return;
+        int ulpA = findParent(a), ulpB = findParent(b);
+        if (ulpA == ulpB) return;
 
-        if (size[pa] < size[pb]) {
-            int tmp = pa;
-            pa = pb;
-            pb = tmp;
+        int sizeA = size[ulpA];
+        int sizeB = size[ulpB];
+
+        if(sizeA < sizeB){
+            int temp = ulpA;
+            ulpA = ulpB;
+            ulpB = temp;
         }
+        
+        size[ulpA] += ulpB;
+        parent[ulpB] = ulpA;
 
-        parent[pb] = pa;
-        size[pa] += size[pb];
+        PriorityQueue<Integer> x = hash.get(ulpA);
+        PriorityQueue<Integer> y = hash.get(ulpB);
 
-        PriorityQueue<Integer> pqA = pqMap.get(pa);
-        PriorityQueue<Integer> pqB = pqMap.get(pb);
-        pqA.addAll(pqB);
-        pqMap.put(pa, pqA);
-        pqMap.remove(pb);
+        x.addAll(y);
+        hash.remove(ulpB);
+        hash.put(ulpA,x);
     }
 
-    void goOffline(int x) {
-        offline.add(x);
+    void offNode(int x) {
+        off.add(x);
     }
 
     int query(int x) {
-        if (!offline.contains(x)) return x;
-
+        if (!off.contains(x)) return x;
         int p = findParent(x);
-        PriorityQueue<Integer> pq = pqMap.get(p);
-
-        while (!pq.isEmpty() && offline.contains(pq.peek())) {
-            pq.poll(); // lazy remove
-        }
-
+        PriorityQueue<Integer> pq = hash.get(p);
+        while (!pq.isEmpty() && off.contains(pq.peek())) pq.poll();
         if (pq.isEmpty()) return -1;
         return pq.peek();
     }
@@ -68,18 +66,13 @@ class Dsu {
 class Solution {
     public int[] processQueries(int c, int[][] connections, int[][] queries) {
         Dsu dsu = new Dsu(c);
-
-        for (int[] edge : connections) {
-            dsu.union(edge[0], edge[1]);
-        }
-
+        for (int[] e : connections) dsu.union(e[0], e[1]);
+        
         List<Integer> ans = new ArrayList<>();
+
         for (int[] q : queries) {
-            if (q[0] == 1) { // query
-                ans.add(dsu.query(q[1]));
-            } else if (q[0] == 2) { // take offline
-                dsu.goOffline(q[1]);
-            }
+            if (q[0] == 1) ans.add(dsu.query(q[1]));
+            else dsu.offNode(q[1]);
         }
 
         return ans.stream().mapToInt(i -> i).toArray();
