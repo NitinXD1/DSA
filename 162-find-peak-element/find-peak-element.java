8d884790144ba87f964eb3d46505f3e626037f3a
Nitin kachera <144806801+NitinXD1@users.noreply.github.com>
2025-11-09 00:42:32 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (5.80%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -5,15 +5,15 @@ class Solution {
 
         while(left < right){
             int mid = left + (right - left)/2;
-
-            if(nums[mid] > nums[mid+1] && nums[mid] > nums[mid+1]){
+            
+            if(nums[mid] > nums[mid+1]){
                 right = mid;
             }
             else{
                 left = mid+1;
             }
         }
-        
+
         return left;
     }
 }
