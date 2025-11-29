9bcf2c09735dbe475082d3b2436b2486d13e0d45
Nitin kachera <144806801+NitinXD1@users.noreply.github.com>
2025-11-29 18:56:36 +0530

    Time: 2 ms (93.36%) | Memory: 47.5 MB (53.63%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,24 +1,21 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        Arrays.sort(nums);
-        int[] ans = new int[2];
         int n = nums.length;
-        Set<Integer> st = new HashSet<>();
-        st.add(nums[0]);
-        for(int i=1;i<n;i++){
-            if(nums[i] == nums[i-1]){
-                ans[0] = nums[i];
+        int[] ans = new int[2];
+        int[] hash = new int[n+1];
+
+        for(int i:nums){
+            hash[i]++;
+            if(hash[i] == 2){
+                ans[0] = i;
             }
-            st.add(nums[i]);
         }
 
-        int cnt = 1;
-        for(int i:nums){
-            if(!st.contains(cnt)){
-                ans[1] = cnt;
+        for(int i=1;i<=n;i++){
+            if(hash[i] == 0){
+                ans[1] = i;
                 break;
             }
-            cnt++;
         }
 
         return ans;
