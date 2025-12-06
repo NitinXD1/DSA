ac87a19b82ee105e1a5570490176d2c96b738081
Nitin kachera <144806801+NitinXD1@users.noreply.github.com>
2025-12-06 16:57:37 +0530

    Time: 22 ms (25.08%) | Memory: 47.4 MB (41.46%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,36 +1,43 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
         int n = grid.length;
+
         if(grid[0][0] == 1 || grid[n-1][n-1] == 1)return -1;
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> Integer.compare(a[2],b[2]));
-        pq.offer(new int[]{0,0,1});
+        Queue<int[]> q = new LinkedList<>();
 
-        int[] drow = {0,1,0,-1,-1,-1,1,1};
-        int[] dcol = {1,0,-1,0,-1,1,1,-1};
+        boolean[][] vis = new boolean[n][n];
 
-        int[][] dist = new int[n][n];
-        for(int[] i:dist)Arrays.fill(i,Integer.MAX_VALUE);
-        dist[0][0] = 1;
+        q.offer(new int[]{0,0});
 
-        while(!pq.isEmpty()){
-            int[] curr = pq.poll();
-            int r = curr[0];
-            int c = curr[1];
-            int s = curr[2];
+        int[] drow = {0,0,-1,-1,-1,1,1,1};
+        int[] dcol = {1,-1,-1,0,1,-1,0,1};
 
-            if(r == n-1 && c == n-1)return s;
-            if(dist[r][c] < s)continue;
+        int cnt = 0;
+        while(!q.isEmpty()){
+            int size = q.size();
+            cnt++;
 
-            for(int i=0;i<8;i++){
-                int adjR = r + drow[i];
-                int adjC = c + dcol[i];
-                
-                if(adjR >= 0 && adjR < n && adjC >= 0 && adjC < n && grid[adjR][adjC] == 0 && s + 1 < dist[adjR][adjC]){
-                    pq.offer(new int[]{adjR,adjC,s+1});
-                    dist[adjR][adjC] = s+1;
+            for(int i=0;i<size;i++){
+                int[] curr = q.poll();
+                int r = curr[0];
+                int c = curr[1];
+
+                if(vis[r][c])continue;
+                vis[r][c] = true;
+
+                if(r== n-1 && c == n-1)return cnt;
+
+                for(int j=0;j<8;j++){
+                    int adjR = r + drow[j];
+                    int adjC = c + dcol[j];
+                    
+                    if(adjR >= 0 && adjR < n && adjC >= 0 && adjC < n && !vis[adjR][adjC] && grid[adjR][adjC] == 0){
+                        q.offer(new int[]{adjR,adjC});
+                    }
                 }
             }
+            
         }
 
         return -1;
