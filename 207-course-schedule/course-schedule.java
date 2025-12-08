cbafbe8d05ada184036d7677625490d25b186009
Nitin kachera <144806801+NitinXD1@users.noreply.github.com>
2025-12-08 13:43:58 +0530

    Time: 4 ms (91.99%) | Memory: 46.9 MB (52.44%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,32 +1,26 @@
 class Solution {
-    public boolean canFinish(int n, int[][] prereq) {
+    public boolean canFinish(int numCourses, int[][] prereq) {
         List<List<Integer>> adj = new ArrayList<>();
-        
-        for(int i=0;i<n;i++){
-            adj.add(new ArrayList<>());
-        }
-
-        for(int[] i:prereq){
-            adj.get(i[1]).add(i[0]);
-        }
+        for(int i=0;i<numCourses;i++)adj.add(new ArrayList<>());
+        for(int[] i:prereq)adj.get(i[1]).add(i[0]);
 
         Stack<Integer> st = new Stack<>();
-        boolean[] vis = new boolean[n];
-        boolean[] currVis = new boolean[n];
+        boolean[] vis = new boolean[numCourses];
+        boolean[] currVis = new boolean[numCourses];
 
-        for(int i=0;i<n;i++){
+        for(int i=0;i<numCourses;i++){
             if(!vis[i]){
                 if(!helper(i,adj,st,vis,currVis))return false;
             }
         }
 
-        return st.size() == n;
+        return true;
     }
 
     public boolean helper(int node,List<List<Integer>> adj,Stack<Integer> st,boolean[] vis,boolean[] currVis){
         vis[node] = true;
         currVis[node] = true;
-        
+
         for(int adjNode : adj.get(node)){
             if(!vis[adjNode]){
                 if(!helper(adjNode,adj,st,vis,currVis))return false;
@@ -36,8 +30,8 @@ class Solution {
             }
         }
 
-        st.add(node);
         currVis[node] = false;
+        st.push(node);
 
         return true;
     }
